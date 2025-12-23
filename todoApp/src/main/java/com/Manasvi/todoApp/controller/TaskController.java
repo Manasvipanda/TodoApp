7ab9c18cd09338d6012fd2410817296a9c965872
Manasvi_panda <manasvipanda10772@gmail.com>
2025-12-23 11:10:51 +0530

    Update TaskController and add backend.log

--- todoApp/src/main/java/com/Manasvi/todoApp/controller/TaskController.java
+++ todoApp/src/main/java/com/Manasvi/todoApp/controller/TaskController.java
@@ -3,11 +3,13 @@ package com.Manasvi.todoApp.controller;
 import com.Manasvi.todoApp.entity.Task;
 import com.Manasvi.todoApp.service.TaskService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
+@CrossOrigin(origins = "http://localhost:5174")
 public class TaskController {
 
     private final TaskService taskService;
@@ -25,7 +27,15 @@ public class TaskController {
     // Get all tasks
     @GetMapping
     public List<Task> getTasks() {
-        return taskService.getAllTasks();
+        System.out.println("Getting all tasks");
+        List<Task> tasks = taskService.getAllTasks();
+        System.out.println("Found " + tasks.size() + " tasks");
+        return tasks;
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
     }
 
     // Update task
